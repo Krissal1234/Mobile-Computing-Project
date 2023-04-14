800d24096316432d9947cc1b1c32403924858519
Krissal1234 <salibakris03@gmail.com>
2023-04-14 11:59:07 +0200

    gets data from firebase, have problems dealing with async task to display the data in the recycler view

--- app/src/main/java/com/example/mobile_computing/ui/flightsPage/FlightsViewModel.java
+++ app/src/main/java/com/example/mobile_computing/ui/flightsPage/FlightsViewModel.java
@@ -1,10 +1,18 @@
 package com.example.mobile_computing.ui.flightsPage;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
 import com.example.mobile_computing.model.FlightDescriptionModel;
+import com.google.firebase.functions.FirebaseFunctions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import services.FirebaseService;
@@ -19,15 +27,67 @@ public class FlightsViewModel extends ViewModel {
         return future;
     }
 
+//    private CompletableFuture<Void> fetchFlightsAsync(String originLocation, String departureDate, String  returnDate) {
+//        FirebaseService service = new FirebaseService();
+//        return CompletableFuture.supplyAsync(() -> service.getTestFlightsEverywhere(originLocation, departureDate, returnDate))
+//                .thenAccept(flightList -> {
+//                    flightData.clear();
+//
+//
+//                    flightData.addAll(flightList);
+//                });
+//    }
+
     private CompletableFuture<Void> fetchFlightsAsync(String originLocation, String departureDate, String  returnDate) {
+        // Get a reference to the Firebase Cloud Function
         FirebaseService service = new FirebaseService();
-        return CompletableFuture.supplyAsync(() -> service.getTestFlightsEverywhere(originLocation, departureDate, returnDate))
+        return CompletableFuture.supplyAsync(() -> getFlightsFirebaseCall())
                 .thenAccept(flightList -> {
                     flightData.clear();
-
-                    //Convert
-
                     flightData.addAll(flightList);
                 });
     }
+
+    private List<FlightDescriptionModel> getFlightsFirebaseCall() {
+        FirebaseFunctions functions = FirebaseFunctions.getInstance();
+
+        // Call the Firebase Cloud Function with the provided parameters
+        try {
+            functions.getHttpsCallable("getFlights")
+                    .call()
+                    .continueWith(task -> {
+                        // Handle the result of the Firebase Cloud Function call
+                        List<FlightDescriptionModel> flightList;
+                        if (!task.isSuccessful()) {
+                            Log.e("Error", task.getException().toString());
+                            throw new Exception(task.getException());
+                        } else {
+                            Log.i("function", task.getResult().getData().toString());
+                            JSONArray jsonArray = new JSONArray(task.getResult().toString());
+
+                            flightList = new ArrayList<>();
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                String country = jsonObject.getString("CountryName");
+                                String price = jsonObject.getString("Price");
+                                String imageUrl = jsonObject.getString("ImageUrl");
+
+                                FlightDescriptionModel flight = new FlightDescriptionModel(country, price, imageUrl);
+                                flightList.add(flight);
+                            }
+                        }
+
+                        return flightList;
+                    });
+        }catch (Exception e) {
+            Log.e("Error", e.toString());
+            return new ArrayList<>();
+        }
+
+        return null;
+
+    }
+
+
 }
--- functions/index.js
+++ functions/index.js
@@ -7,5 +7,32 @@ const functions = require("firebase-functions");
    functions.logger.info("Hello logs!");
    response.send("Hello from Firebase!");
  });
+exports.getFlights = functions.https.onCall(async (data) => {
+
+//    const originLocation = data.originLocation;
+//    const departureDate = data.departureDate;
+//    const returnDate = data.returnDate;
+
+    const cheapestFlights = [
+        {
+            "CountryName": "France",
+            "Price": "12",
+            "ImageUrl": "https://content.skyscnr.com/c88da2b091534f7baf8536b3959ce83a/GettyImages-495057957.jpg?crop=400px:400px&quality=75"
+        },
+        {
+            "CountryName": "Italy",
+            "Price": "14",
+            "imageUrl": "https://content.skyscnr.com/f348d79cfdf70286dc759d24618a23c3/GettyImages-182281845.jpg?crop=400px:400px&quality=75"
+        },
+        {
+            "CountryName": "Spain",
+            "Price": "15",
+            "ImageUrl": "https://content.skyscnr.com/e0241c97c2b33b71e9c278bc89bb17ed/GettyImages-178640523.jpg?crop=400px:400px&quality=75"
+        }
+    ];
+
+    return cheapestFlights;
+});
+
 
  //Firebase function to get flights depending on the origin location, departure and return date.
