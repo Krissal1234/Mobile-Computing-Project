b496b799d76bb3146fbd4a64a48ded24716552d3
Krissal1234 <salibakris03@gmail.com>
2023-05-18 13:22:52 +0200

    small changes

--- app/src/main/java/com/example/mobile_computing/ui/flightsPage/FlightApi.java
+++ app/src/main/java/com/example/mobile_computing/ui/flightsPage/FlightApi.java
@@ -8,6 +8,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface FlightApi {
+    //TODO Since the api is hosted locally, the host's IP address must be switched out instead of "localhost"
     String BASE_URL = "http://192.168.1.138:3000/";
     @GET("flights")
     Call<List<FlightModel>> getFlights(@Query("origin") String origin, @Query("departureDate") String departureDate,@Query("returnDate") String returnDate);
