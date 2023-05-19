dc7b237a93f2116fa5602d7dbbc7ba3fa04f8f46
Krissal1234 <salibakris03@gmail.com>
2023-05-19 16:05:10 +0200

    change of api port

--- app/src/main/java/com/example/mobile_computing/ui/tripsPage/HotelApi.java
+++ app/src/main/java/com/example/mobile_computing/ui/tripsPage/HotelApi.java
@@ -16,7 +16,7 @@ public interface HotelApi {
      * The base URL of the Hotel API.
      * TODO: Replace "<host's IP here>" with the actual IP address of the host where the API is hosted.
      */
-    String BASE_URL = "http://<host's IP here>:3000/";
+    String BASE_URL = "http://<host's IP here>:8080/";
     @GET("hotels")
     Call<List<HotelModel>> getHotels(@Query("arrivalDate") String checkIn, @Query("departureDate") String checkOut, @Query("country") String destination);
 }
